d7ffa1c0631a0a6c9d2ca08ff94e4bf49aada86f
elaksc <elakascioglu3425@gmail.com>
2022-11-10 15:40:38 +0300

    new practice has been added

---
+++ UsALma/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		int us,sayi;
+		
+		
+		System.out.println("Sayý Giriniz: ");
+		sayi = inp.nextInt();
+		
+		System.out.println("Us sayisi: ");
+		us = inp.nextInt();
+		
+		int toplam = 1;
+		for(int i = 1; i<=us; i++) {
+			toplam *= sayi;
+		}
+	}
+}
