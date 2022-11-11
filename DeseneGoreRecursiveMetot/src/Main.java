96bc91ba27c7deab277208ce40936895cd5dd9d2
elaksc <elakascioglu3425@gmail.com>
2022-11-11 14:16:24 +0300

    update

--- DeseneGoreRecursiveMetot/src/Main.java
+++ DeseneGoreRecursiveMetot/src/Main.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
 		System.out.println("Sayi Giriniz: ");
 		int number = inp.nextInt();
-		int result = number ;
+		int result = number;
 		int tempNumber = number;
-		
-		if(number == 0 ) {
+
+		if (number == 0) {
 			System.out.println("0 Kullanılamaz");
-		}
-		else {
-			pattern(number,result,tempNumber);
+		} else {
+			pattern(number, result, tempNumber);
 		}
 	}
+
 	static void pattern(int number, int result, int tempNumber) {
-		if(tempNumber <= 0) {
+		if (tempNumber <= 0) {
 			System.out.print(result + " ");
 			result += 5;
-			if(result == number) {
+			if (result == number) {
 				System.out.print(result + " ");
 				return;
 			}
-		}else if(tempNumber > 0) {
+		} else if (tempNumber > 0) {
 			System.out.print(result + " ");
 			result -= 5;
 			if (result <= 0) {
 				tempNumber = result;
 			}
 		}
-		pattern(number,result,tempNumber);
+		pattern(number, result, tempNumber);
 	}
-	
 
 }
