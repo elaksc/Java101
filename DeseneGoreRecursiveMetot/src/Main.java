95ae65f87981fa249ac018c06908d8d265dbd647
elaksc <elakascioglu3425@gmail.com>
2022-11-11 14:15:09 +0300

    new practice has been added

---
+++ DeseneGoreRecursiveMetot/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Sayi Giriniz: ");
+		int number = inp.nextInt();
+		int result = number ;
+		int tempNumber = number;
+		
+		if(number == 0 ) {
+			System.out.println("0 Kullanılamaz");
+		}
+		else {
+			pattern(number,result,tempNumber);
+		}
+	}
+	static void pattern(int number, int result, int tempNumber) {
+		if(tempNumber <= 0) {
+			System.out.print(result + " ");
+			result += 5;
+			if(result == number) {
+				System.out.print(result + " ");
+				return;
+			}
+		}else if(tempNumber > 0) {
+			System.out.print(result + " ");
+			result -= 5;
+			if (result <= 0) {
+				tempNumber = result;
+			}
+		}
+		pattern(number,result,tempNumber);
+	}
+	
+
+}
